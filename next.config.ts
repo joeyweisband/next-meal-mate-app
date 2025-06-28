453564e2f9db70b3125a980dcf270f91024c4a14
joeyweisband <josephweisband+gh@gmail.com>
2025-06-28 08:15:32 -0500

    fix next config errors

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
 import type { NextConfig } from "next";
-import { FlatCompat } from '@eslint/eslintrc'
- 
-const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-})
 
 const nextConfig: NextConfig = {
   images: {
@@ -18,15 +12,5 @@ const nextConfig: NextConfig = {
     ],
   },
 };
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ['next'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
-  }),
-]
 
 export default nextConfig;
