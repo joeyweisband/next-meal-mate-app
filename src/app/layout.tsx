989275adaea804d4585a18c8998967214d24b627
joeyweisband <josephweisband+gh@gmail.com>
2025-08-26 00:22:19 -0500

    try to fix color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -120,6 +120,7 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#27ae60" />
+        <meta name="color-scheme" content="light" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Meal Mate" />
