5d4e70313f9a35cb3a7ce63c48f2679f7af3ad03
joeyweisband <josephweisband+gh@gmail.com>
2025-11-07 02:07:45 -0600

    change background to white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -119,7 +119,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#27ae60" />
+        <meta name="theme-color" content="#ffffff" />
         <meta name="color-scheme" content="light" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
